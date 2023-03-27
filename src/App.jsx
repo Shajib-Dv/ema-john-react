b215394b1fe007efb2a03ee9081d3dd8ed28d87b
Shajib-Dv <mdsojeeb242@gmail.com>
2023-03-27 14:32:32 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+const Header = () => {
+  return (
+    <>
+      <nav className="header">
+        <img src={logo} alt="" />
+        <div className="nav-items">
+          <a href="/order">Order</a>
+          <a href="/orderReview">Order Review</a>
+          <a href="/manageInventory">Manage Inventory</a>
+          <a href="/lonIn">Log In</a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
